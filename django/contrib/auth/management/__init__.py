2c8b13f5386377631b8433e6578359bbb809bf6e
feiwang <wangfei@chunyu.me>
2015-06-16 17:34:48 +0800

    优化了Create Permission的操作

--- django/contrib/auth/management/__init__.py
+++ django/contrib/auth/management/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 """
 Creates permissions for all installed apps that need permissions.
 """
@@ -12,14 +13,22 @@ def _get_permission_codename(action, opts):
 def _get_all_permissions(opts):
     "Returns (codename, name) for all permissions in the given opts."
     perms = []
+
+    # 添加固定的权限
     for action in ('add', 'change', 'delete', 'view'):
         perms.append((_get_permission_codename(action, opts), u'Can %s %s' % (action, opts.verbose_name_raw)))
+
+    # 自定义的权限
     return perms + list(opts.permissions)
+
+all_perms = None
 
 def create_permissions(app, created_models, verbosity, **kwargs):
     from django.contrib.contenttypes.models import ContentType
 
     app_models = get_models(app)
+
+    # print "create_permissions--------->"
 
     # This will hold the permissions we're looking for as
     # (content_type, (codename, name))
@@ -30,21 +39,22 @@ def create_permissions(app, created_models, verbosity, **kwargs):
         ctype = ContentType.objects.get_for_model(klass)
         ctypes.add(ctype)
         for perm in _get_all_permissions(klass._meta):
-            searched_perms.append((ctype, perm))
+            searched_perms.append((ctype, perm)) # 将要生成的Permissions
 
     # Find all the Permissions that have a context_type for a model we're
     # looking for.  We don't need to check for codenames since we already have
     # a list of the ones we're going to create.
-    all_perms = set(auth_app.Permission.objects.filter(
-        content_type__in=ctypes,
-    ).values_list(
-        "content_type", "codename"
-    ))
+    global all_perms
+    if not all_perms:
+        # 一口气读取所有的Permission(假设Permission的个数不多)
+        all_perms = set(auth_app.Permission.objects.all().values_list("content_type", "codename"))
 
     for ctype, (codename, name) in searched_perms:
         # If the permissions exists, move on.
+        # codename比较紧凑
         if (ctype.pk, codename) in all_perms:
             continue
+        # print "Add New Permission: ", ctype, codename, name
         p = auth_app.Permission.objects.create(
             codename=codename,
             name=name,
@@ -70,7 +80,6 @@ def create_superuser(app, created_models, verbosity, **kwargs):
                 call_command("createsuperuser", interactive=True)
             break
 
-signals.post_syncdb.connect(create_permissions,
-    dispatch_uid = "django.contrib.auth.management.create_permissions")
-signals.post_syncdb.connect(create_superuser,
-    sender=auth_app, dispatch_uid = "django.contrib.auth.management.create_superuser")
+# 在post_syncdb中调用 create_permissions
+signals.post_syncdb.connect(create_permissions,dispatch_uid = "django.contrib.auth.management.create_permissions")
+signals.post_syncdb.connect(create_superuser, sender=auth_app, dispatch_uid = "django.contrib.auth.management.create_superuser")
